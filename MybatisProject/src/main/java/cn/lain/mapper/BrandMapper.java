ec072dd483e7d032e1f28019d52a96e326d27aa5
Lain-Git <lianer10062021@163.com>
2022-12-21 09:34:28 +0800

    修改了BrandMapper.java文件

--- MybatisProject/src/main/java/cn/lain/mapper/BrandMapper.java
+++ MybatisProject/src/main/java/cn/lain/mapper/BrandMapper.java
@@ -1,8 +1,10 @@
 package cn.lain.mapper;
 
 import cn.lain.pojo.Brand;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface BrandMapper {
 
@@ -10,4 +12,29 @@ public interface BrandMapper {
      * 查询所有
      */
     public List<Brand> selectAll();
+
+    /**
+     * 查看详情：根据id查询
+     */
+    Brand selectById(int id);
+
+    /**
+     * 条件查询
+     * * 参数接收
+     *      1. 散装参数：如果方法中有多个参数，需要使用@param("sql参数占位符名称")
+     *      2. 对象参数：对象的属性名称要和参数占位符一致
+     *      3. map集合参数
+     *
+     * @param status
+     * @param companyName
+     * @param brandName
+     * @return
+     */
+    List<Brand> selectByCondition(@Param("status") int status,
+                                  @Param("companyName") String companyName,
+                                  @Param("brandName") String brandName);
+
+//    List<Brand> selectByCondition(Brand brand);
+
+//    List<Brand> selectByCondition(Map map);
 }
